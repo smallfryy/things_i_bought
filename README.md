1012c41140b2faf89f83d4cc76ba954395291fe7
smallfryy <holly.m.peck@gmail.com>
2016-03-05 17:42:27 -0500

    added web app framework with rails

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # things_i_bought
-I am building a Ruby on Rails web app for learning purposes. 
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ThingsIBought::Application.config.secret_key_base = '63971e3708a5fa5a8ee26b0828747c42108d294f0f1334d8148ea0dced5e41cb3436127a270c25ee8df206fbda4573a52718c08420b5996d9e999899f63c5a0b'
